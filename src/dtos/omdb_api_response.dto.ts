49a8a8c2226f7e3cd16a67b89af63edd8b9dfd8c
Mateusz Sawastian <m.sawastian@gmail.com>
2020-01-27 21:57:04 +0100

    Added OMDB API response DTO, custom error and Response transformer

---
+++ src/dtos/omdb_api_response.dto.ts
@@ -0,0 +1,130 @@
+import { Expose, Exclude } from 'class-transformer';
+import { IsDefined, IsEmpty, IsNumberString, IsString, IsUrl, IsEmail, IsNumber, IsBooleanString } from 'class-validator';
+
+
+export class OmdbApiResponseDTO {
+
+  @IsDefined()
+  @IsString()
+  @Expose()
+  Title: string;
+
+  @IsDefined()
+  @IsString()
+  @Expose()
+  Year: string;
+
+  @IsDefined()
+  @IsString()
+  @Expose()
+  Rated: string;
+
+  @IsDefined()
+  @IsString()
+  @Expose()
+  Released: string;
+
+  @IsDefined()
+  @IsString()
+  @Expose()
+  Runtime: string;
+
+  @IsDefined()
+  @IsString()
+  @Expose()
+  Genre: string;
+
+  @IsDefined()
+  @IsString()
+  @Expose()
+  Director: string;
+
+  @IsDefined()
+  @IsString()
+  @Expose()
+  Writer: string;
+
+  @IsDefined()
+  @IsString()
+  @Expose()
+  Actors: string;
+
+  @IsDefined()
+  @IsString()
+  @Expose()
+  Plot: string;
+
+  @IsDefined()
+  @IsString()
+  @Expose()
+  Language: string;
+
+  @IsDefined()
+  @IsString()
+  @Expose()
+  Country: string;
+
+  @IsDefined()
+  @IsString()
+  @Expose()
+  Awards: string;
+
+  @IsDefined()
+  @IsEmail()
+  @Expose()
+  Poster: string;
+
+  @IsDefined()
+  @IsNumberString()
+  @Expose()
+  Metascore: string;
+
+  @IsDefined()
+  @IsNumberString()
+  @Expose()
+  imdbRating: string;
+
+  @IsDefined()
+  @IsString()
+  @Expose()
+  imdbVotes: string;
+
+  @IsDefined()
+  @IsString()
+  @Expose()
+  imdbId: string;
+
+  @IsDefined()
+  @IsString()
+  @Expose()
+  Type: string;
+
+  @IsDefined()
+  @IsString()
+  @Expose()
+  DVD: string;
+
+  @IsDefined()
+  @IsString()
+  @Expose()
+  BoxOffice: string;
+  
+  @IsDefined()
+  @IsString()
+  @Expose()
+  Production: string;
+
+  @IsDefined()
+  @IsString()
+  @Expose()
+  Website: string;
+
+  @IsDefined()
+  @IsBooleanString()
+  @Exclude()
+  Response: string;
+
+  @IsEmpty()
+  @Exclude()
+  readonly Error?: string;
+}
---
+++ src/omdb/responder/transformer.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from "@nestjs/common";
+import { OmdbApiResponseDTO } from "src/dtos/omdb_api_response.dto";
+import { MovieDTO } from "src/dtos/movie.dto";
+
+@Injectable()
+export class TransformerService {
+
+  constructor(private readonly nullString: string) {}
+
+  private parseNull(str: string): string | null {
+    return str === this.nullString ? null : str;
+  }
+
+  private parseInt(str: string): number | null {
+    const parsedInt = parseInt(str, 10);
+    return Number.isFinite(parsedInt) ? parsedInt : null;
+  }
+
+  private parseFloat(str: string): number | null {
+    const parsedFloat = this.parseFloat(str);
+    return Number.isFinite(parsedFloat) ? parsedFloat : null;
+  }
+
+  private parseDecimalSeparator(str: string): string {
+    return str.replace(/,/g, '');
+  }
+
+  transform(omdbRes: OmdbApiResponseDTO): MovieDTO {
+    return new MovieDTO(
+      {
+        imdbID: omdbRes.imdbId,
+        title: omdbRes.Title,
+        year: this.parseNull(omdbRes.Year),
+        rated: this.parseNull(omdbRes.Rated),
+        released: this.parseNull(omdbRes.Released),
+        genre: this.parseNull(omdbRes.Genre),
+        director: this.parseNull(omdbRes.Director),
+        writer: this.parseNull(omdbRes.Writer),
+        actors: this.parseNull(omdbRes.Actors),
+        plot: this.parseNull(omdbRes.Plot),
+        language: this.parseNull(omdbRes.Language),
+        country: this.parseNull(omdbRes.Country),
+        awards: this.parseNull(omdbRes.Awards),
+        poster: this.parseNull(omdbRes.Poster),
+        metascore: this.parseInt(omdbRes.Metascore),
+        imdbRating: this.parseFloat(omdbRes.imdbRating),
+        imdbVotes: this.parseInt(this.parseDecimalSeparator(omdbRes.imdbVotes)),
+        type: this.parseNull(omdbRes.Type),
+        dvd: this.parseNull(omdbRes.DVD),
+        boxOffice: this.parseNull(omdbRes.BoxOffice),
+        production: this.parseNull(omdbRes.Production),
+        website: this.parseNull(omdbRes.Website)
+      }
+    );
+  }
+  
+}
