ac00f7678b5225db012dda6de3dd492860c7063f
Mateusz Sawastian <m.sawastian@gmail.com>
2020-04-01 14:57:56 +0200

    Added per-line linting with Prettier. Added missing Auth decorators.

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,24 +1,28 @@
-import { Controller, Get, Post, UsePipes, ValidationPipe, Body, Delete, Param, HttpCode, HttpStatus } from "@nestjs/common";
+import { Controller, Get, Post, UsePipes, ValidationPipe, Body, Delete, Param, HttpCode, HttpStatus, UseGuards } from "@nestjs/common";
 import { CommentsService } from "./comments.service";
 import { CommentDTO } from "~dtos/comment/comment.dto";
 import { SaveCommentDTO } from "~dtos/comment/save_comment.dto";
+import { JwtAuthGuard } from "~auth/guards/jwt-auth.guard";
 
 @Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) { }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   async fetchAll(): Promise<CommentDTO[]> {
     const comments = await this.commentsService.fetchAll();
     return Promise.all(comments.map(comment => comment.serialize()));
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   @UsePipes(new ValidationPipe())
   async save(@Body() saveCommentDTO: SaveCommentDTO) {
     await this.commentsService.save(saveCommentDTO);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete('/:id')
   @HttpCode(HttpStatus.NO_CONTENT)
   async remove(@Param('id') id: number) {
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, HttpCode, HttpStatus, UsePipes, ValidationPipe, Body } from "@nestjs/common";
+import { Controller, Get, Post, HttpCode, HttpStatus, UsePipes, ValidationPipe, Body, UseGuards } from "@nestjs/common";
 import { MoviesService } from "./movies.service";
 import { MovieDTO } from "~dtos/movie/movie.dto";
 import { AddMovieDTO } from "~dtos/movie/add_movie.dto";
 import { OmdbClientService } from "~omdb/client/omdb_client.service";
+import { JwtAuthGuard } from "~auth/guards/jwt-auth.guard";
 
 @Controller('movies')
 export class MoviesController {
@@ -12,12 +13,14 @@ export class MoviesController {
     private readonly omdbClientService: OmdbClientService,
   ) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   async fetchAll(): Promise<MovieDTO[]> {
     const movies = await this.movieService.fetchAll();
     return movies.map(movie => movie.serialize());
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   @HttpCode(HttpStatus.ACCEPTED)
   @UsePipes(new ValidationPipe())
